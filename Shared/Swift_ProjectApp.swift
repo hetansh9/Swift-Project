22ebd5a57d8eaa8b9db52c4c5bf12cac692a3eeb
Hetansh Patel <hetansh.patel@icloud.com>
2021-05-17 16:56:18 -0600

    Initial Commit

---
+++ Shared/Swift_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ProjectApp.swift
+//  Shared
+//
+//  Created by Hetansh on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

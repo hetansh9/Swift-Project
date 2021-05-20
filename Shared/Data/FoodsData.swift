cacbd298bebe44f9f4a999892fd4a7eeaccb0559
Het Prajapati <hprajapati@albany.edu>
2021-05-20 10:28:16 +0530

    data file

---
+++ Shared/Data/FoodsData.swift
@@ -0,0 +1,23 @@
+//
+//  FoodsData.swift
+//  Swift Project (iOS)
+//
+//  Created by Het Prajapati on 5/20/21.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - FOOD DATA
+
+let foodsData: [Food] = [
+    
+    // Het
+
+    Food(title: "Explore", headline: "Foods", image: "breakfast", gradientColors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], description: "This is a test description", nutrition: "akjcs"),
+
+    Food(title: "Explore", headline: "Foods", image: "breakfast", gradientColors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], description: "This is a test description", nutrition: "kjasbckjbc")
+    
+    // Hetansh
+
+]
